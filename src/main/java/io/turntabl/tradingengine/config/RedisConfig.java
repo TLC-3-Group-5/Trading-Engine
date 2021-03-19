9622bbe3fee76b00011619b7764a56d29eb1f82e
Smart <kwesi.smart@turntabl.io>
2021-03-19 13:02:08 +0000

    Configure CI/CD

--- src/main/java/io/turntabl/tradingengine/config/RedisConfig.java
+++ src/main/java/io/turntabl/tradingengine/config/RedisConfig.java
@@ -1,8 +1,13 @@
 package io.turntabl.tradingengine.config;
 
 import io.turntabl.tradingengine.subscriber.Receiver;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -11,20 +16,26 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
-
 @Configuration
 public class RedisConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean
-    public JedisConnectionFactory connectionFactory(){
+    public JedisConnectionFactory connectionFactory() {
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName("localhost");
-        configuration.setPort(6379);
-        return  new JedisConnectionFactory(configuration);
+
+        configuration.setHostName(Optional.ofNullable(env.getProperty("app.SPRING_REDIS_URL")).orElse(""));
+        configuration.setPort(Integer.parseInt(env.getProperty("app.SPRING_REDIS_PORT")));
+        configuration.setUsername(env.getProperty("app.SPRING_REDIS_USER"));
+        configuration.setPassword(env.getProperty("app.SPRING_REDIS_PASS"));
+
+        return new JedisConnectionFactory(configuration);
     }
 
     @Bean
-    public RedisTemplate<String, Object> template(){
+    public RedisTemplate<String, Object> template() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory());
         template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
@@ -32,17 +43,17 @@ public class RedisConfig {
     }
 
     @Bean
-    public ChannelTopic topic(){
-       return new ChannelTopic("orders");
+    public ChannelTopic topic() {
+        return new ChannelTopic("orders");
     }
 
     @Bean
-    public MessageListenerAdapter messageListenerAdapter(){
+    public MessageListenerAdapter messageListenerAdapter() {
         return new MessageListenerAdapter(new Receiver());
     }
 
     @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer(){
+    public RedisMessageListenerContainer redisMessageListenerContainer() {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory());
         container.addMessageListener(messageListenerAdapter(), topic());
