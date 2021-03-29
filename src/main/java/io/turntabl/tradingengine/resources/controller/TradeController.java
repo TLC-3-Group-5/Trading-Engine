1422d5aa8c6acad0b104fc82db6fb740171581cb
Smart <kwesi.smart@turntabl.io>
2021-03-29 13:04:39 +0000

    Add feature to get all trades by order id

--- src/main/java/io/turntabl/tradingengine/resources/controller/TradeController.java
+++ src/main/java/io/turntabl/tradingengine/resources/controller/TradeController.java
@@ -1,7 +1,9 @@
 package io.turntabl.tradingengine.resources.controller;
 
+import io.turntabl.tradingengine.resources.model.Response;
 import io.turntabl.tradingengine.resources.service.TradeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +23,16 @@ public class TradeController {
     @PutMapping(path="/{tradeId}/{exchangeId}")
     public void updateTrade(@PathVariable String tradeId, @PathVariable String exchangeId){
         tradeService.updateTrade(tradeId, exchangeId);
+    }
+
+    // /trade?order_id=xxx
+    @GetMapping
+    public Response getTradesPerOrder(@PathVariable(name = "order_id") Long orderId){
+        var res = new Response();
+
+        res.setData(tradeService.getTradesPerOrder(orderId));
+        res.setCode(200);
+
+        return res;
     }
 }
