53b829dd4f1b9c225a312b8f90f26463f4b4aea8
Smart <kwesi.smart@turntabl.io>
2021-03-30 12:45:18 +0000

    Disable CORS

---
+++ src/main/java/io/turntabl/tradingengine/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package io.turntabl.producer.resources.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
